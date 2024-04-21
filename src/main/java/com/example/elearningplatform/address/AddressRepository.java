3795efad3c7937cbb2bea7a15bfbbbba0069acc6
Mohamed Reda soliman zahw <112642921+mohammedzahw@users.noreply.github.com>
2024-04-21 23:23:26 +0200

    Delete src/main directory

--- src/main/java/com/example/elearningplatform/address/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.elearningplatform.address;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.elearningplatform.user.User;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-    public Address findByUser(User user);
-}
--- src/main/java/com/example/elearningplatform/user/Role.java
+++
@@ -1,9 +0,0 @@
-package com.example.elearningplatform.user;
-
-import jakarta.persistence.Entity;
-
-public enum Role {
-
-    ROLE_USER, ROLE_ADMIN, ROLE_INSTRUCTOR, ROLE_STUDENT
-
-}
